7e05351e8ab5a14716bd40938a1829821d09534e
Guilherme Moura <guilhermecourty@gmail.com>
2024-05-19 17:28:47 -0300

    added examples

--- app.py
+++ app.py
@@ -39,3 +39,50 @@ if st.button('Classificar atendimento'):
         st.write('Não foi possível classificar o texto. Por favor, tente novamente.')
         # Print error
         st.write(response.text)
+
+# Sidebar for category selection
+st.sidebar.header("Precisa de ajuda?")
+category = st.sidebar.selectbox(
+    "Categorias exemplo",
+    ["Escolha uma Categoria",
+    "Hipotecas / Empréstimos",
+    "Roubo / Relatório de disputa",
+    "Serviços de conta bancária"
+    ]
+)
+
+def display_example(text, background_color="#f0f2f6", text_color="#333"):
+    """
+    Display the example text in a styled bubble.
+    Args:
+    - text: The example text to display.
+    - background_color: The background color of the bubble.
+    - text_color: The text color.
+    """
+    html = f"""
+    <div style='background-color: {background_color}; border-radius: 10px; padding: 10px; margin: 10px 0;'>
+    <p style='color: {text_color}; margin:0;'>{text}</p>
+    </div>
+    """
+    st.markdown(html, unsafe_allow_html=True)
+
+# Example texts mapped to categories
+example_texts = {
+    "Hipotecas / Empréstimos": "Estou tentando refinanciar minha hipoteca há meses, mas não \
+        consigo obter uma taxa de juros satisfatória. Preciso de orientação sobre como proceder \
+            para conseguir uma melhor oferta.",
+    "Roubo / Relatório de disputa": "Meu cartão de crédito foi clonado e observei transações \
+        não autorizadas. Como posso contestar essas cobranças e obter um novo cartão com segurança \
+            reforçada?",
+    "Serviços de conta bancária": "Houve um problema com a transferência que tentei fazer para outra \
+        conta. O valor foi debitado da minha conta, mas o destinatário não recebeu o dinheiro. \
+            Preciso de ajuda urgente para resolver essa questão."
+}
+
+# Main area: Displaying the example based on selection
+st.header("Exemplo de texto para ser Predito")
+if category and category != "Escolha uma Categoria":
+    st.write(f"Exemplo para **{category}**:")
+    display_example(example_texts[category])
+else:
+    st.write("Seleciona uma Categoria de Chamados para ver o texto de exemplo.")
